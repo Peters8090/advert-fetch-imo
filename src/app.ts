6a621cf8a29f4f32c44f46831640aba501d81449
Piotr Bartoszewski <bartoszewskip1000@gmail.com>
2020-11-17 15:54:00 +0100

    Add for loop iterating over the unpacked files + add 'added-to-db.txt' file support

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import extract from "extract-zip";
 import fs from "promise-fs";
 import lodash from "lodash";
 
+console.time();
 (async () => {
   const UNPACKED_ADVERTS_DIR = "adverts_unpacked";
   const PACKED_ADVERTS_DIR = "adverts_packed";
@@ -23,4 +24,19 @@ import lodash from "lodash";
       dir: `${process.cwd()}/${UNPACKED_ADVERTS_DIR}/${packedFile}`,
     });
   }
+
+  const unpackedFiles = await fs.readdir(UNPACKED_ADVERTS_DIR);
+  for (const unpackedFile of unpackedFiles) {
+    const ADDED_TO_DB_FILE_NAME = "added-to-db.txt";
+
+    if (
+      !(await fs.readdir(`${UNPACKED_ADVERTS_DIR}/${unpackedFile}`)).includes(
+        ADDED_TO_DB_FILE_NAME
+      )
+    ) {
+      // not added to db
+    }
+  }
+
+  console.timeEnd();
 })();
